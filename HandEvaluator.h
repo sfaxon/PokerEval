c6f93998844351254bfa9cae7902fa37e4627bc4
Seth Faxon <seth.faxon@gmail.com>
2012-06-17 18:44:10 -0700

    adding HandEvaluator class

---
+++ HandEvaluator.h
@@ -0,0 +1,13 @@
+//
+//  HandEvaluator.h
+//  PokerEval
+//
+//  Created by Seth Faxon on 6/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HandEvaluator : NSObject
+
+@end
---
+++ HandEvaluator.m
@@ -0,0 +1,13 @@
+//
+//  HandEvaluator.m
+//  PokerEval
+//
+//  Created by Seth Faxon on 6/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "HandEvaluator.h"
+
+@implementation HandEvaluator
+
+@end
