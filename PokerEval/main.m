e1052142af8a89d95845c1272b4ec86a9e8a9a95
Seth Faxon <seth.faxon@gmail.com>
2012-06-17 18:32:23 -0700

    Initial Commit

---
+++ PokerEval/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PokerEval
+//
+//  Created by Seth Faxon on 6/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
